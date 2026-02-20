baee1a1e4a91229830e322a9f55d8ef4d4ce1801
Eloy Marín <eloymc16@gmail.com>
2026-02-20 20:03:54 +0100

    restore admin emails list

--- server/auth.js
+++ server/auth.js
@@ -405,7 +405,9 @@ export async function getUserFromSession(sessionToken) {
 
 // Admin/test emails that get unlimited credits
 const ADMIN_EMAILS = [
-  'admin@intrview.io' // Add your email here
+  'admin@intrview.io',
+  'test@intrview.io',
+  'alberto@intrview.io' // Add your email here
 ];
 
 // Check if user is admin
